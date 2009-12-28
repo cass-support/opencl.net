6c1c4b6adec04c7ab6154f06526eeedf3c53a350
Mordechai Botrashvily <support@cass-hpc.com>
2009-12-28 09:45:22 +0000

    - Created version 1.0.48.1. - Minor addition. - Changed Vender to Vendor in platform info enum.

--- OpenCL.NET/Driver/Types.cs
+++ OpenCL.NET/Driver/Types.cs
@@ -130,7 +130,7 @@ namespace GASS.OpenCL
         Profile = 0x0900,
         Version = 0x0901,
         Name = 0x0902,
-        Vender = 0x0903,
+        Vendor = 0x0903,
         Extensions = 0x0904,
     }
 
