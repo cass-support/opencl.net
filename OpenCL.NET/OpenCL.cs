5ebe40557e39d4960af0b4a3b94cc2f98419013c
Mordechai Botrashvily <support@cass-hpc.com>
2012-10-11 18:14:06 +0000

    - Added OpenCLException class to represent runtime errors. - Added OO class OpenCL. - First implementation of static function to handle Platform functions.

---
+++ OpenCL.NET/OpenCL.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using CASS.Types;
+using System.Runtime.InteropServices;
+
+namespace CASS.OpenCL
+{
+    /// <summary>
+    /// This class provides object oriented access to OpenCL(TM) driver API 
+    /// and further utilities for simpler programming.
+    /// </summary>
+    public class OpenCL
+    {
+        #region Platform Utilities
+        /// <summary>
+        /// Returns an array of available platforms in the system.
+        /// If no platform is found, an array with zero elements is returned.
+        /// </summary>
+        /// <returns>An array of available platforms in the system.</returns>
+        public static CLPlatformID[] GetPlatforms()
+        {
+            // Check how many platforms are available.
+            uint num_platforms = 0;
+            CLError err = OpenCLDriver.clGetPlatformIDs(0, IntPtr.Zero, ref num_platforms);
+
+            if (err != CLError.Success)
+            {
+                throw new OpenCLException(err);
+            }
+
+            if (num_platforms < 1)
+            {
+                return new CLPlatformID[0];
+            }
+
+            // Get the actual platforms once we know their amount.
+            CLPlatformID[] platforms = new CLPlatformID[num_platforms];
+            err = OpenCLDriver.clGetPlatformIDs(num_platforms, platforms, ref num_platforms);
+
+            return platforms;
+        }
+
+        public static object GetPlatformInfo(CLPlatformID platform, CLPlatformInfo info)
+        {
+            CLError err = CLError.Success;
+
+            // Define variables to store native information.
+            SizeT param_value_size_ret = 0;
+            IntPtr ptr = IntPtr.Zero;
+            object result = null;
+
+            // Get initial size of buffer to allocate.
+            err = OpenCLDriver.clGetPlatformInfo(platform, info, 0, IntPtr.Zero, ref param_value_size_ret);
+
+            if (err != CLError.Success)
+            {
+                throw new OpenCLException(err);
+            }
+
+            if (param_value_size_ret < 1)
+            {
+                return result;
+            }
+
+            // Allocate native memory to store value.
+            ptr = Marshal.AllocHGlobal(param_value_size_ret);
+
+            // Protect following statements with try-finally in case something 
+            // goes wrong.
+            try
+            {
+                // Get actual value.
+                err = OpenCLDriver.clGetPlatformInfo(platform, info,
+                param_value_size_ret, ptr, ref param_value_size_ret);
+
+                switch (info)
+                {
+                    case CLPlatformInfo.Profile:
+                        result = Marshal.PtrToStringAnsi(ptr, param_value_size_ret);
+                        break;
+                    case CLPlatformInfo.Version:
+                        result = Marshal.PtrToStringAnsi(ptr, param_value_size_ret);
+                        break;
+                    case CLPlatformInfo.Name:
+                        result = Marshal.PtrToStringAnsi(ptr, param_value_size_ret);
+                        break;
+                    case CLPlatformInfo.Vendor:
+                        result = Marshal.PtrToStringAnsi(ptr, param_value_size_ret);
+                        break;
+                    case CLPlatformInfo.Extensions:
+                        result = Marshal.PtrToStringAnsi(ptr, param_value_size_ret);
+                        break;
+                }
+            }
+            finally
+            {
+                // Free native buffer.
+                Marshal.FreeHGlobal(ptr);
+            }
+
+            throw new OpenCLException(CLError.MapFailure);
+
+            return result;
+        }
+        #endregion
+        
+    }
+}
---
+++ OpenCL.NET/OpenCLException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CASS.OpenCL
+{
+    /// <summary>
+    /// Represents an OpenCL(TM) exception that occured in one of the API functions.
+    /// </summary>
+    public class OpenCLException : Exception
+    {
+        /// <summary>
+        /// Creates a new exception instance with given error code.
+        /// </summary>
+        /// <param name="error">OpenCL(TM) error code.</param>
+        public OpenCLException(CLError error)
+        {
+            OpenCLErrorCode = error;
+        }
+
+        /// <summary>
+        /// Gets OpenCL(TM) error string and code.
+        /// </summary>
+        public override string Message
+        {
+            get
+            {
+                return string.Format("OpenCL error {0} (Code: {1})", OpenCLErrorCode, (int)OpenCLErrorCode);
+            }
+        }
+
+        /// <summary>
+        /// OpenCL(TM) error code.
+        /// </summary>
+        public CLError OpenCLErrorCode { get; private set; }
+    }
+}
