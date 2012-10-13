b1a0ae422b241158e9515a464985efc04412c0ce
Mordechai Botrashvily <support@cass-hpc.com>
2012-10-13 19:09:31 +0000

    Added initial support for memory functions.

--- OpenCL.NET/OpenCL.cs
+++ OpenCL.NET/OpenCL.cs
@@ -133,6 +133,75 @@ namespace CASS.OpenCL
         }
         #endregion
 
+        #region Memory Functions
+        public CLMem CreateBuffer(SizeT sizeInBytes)
+        {
+            return CreateBuffer(CLMemFlags.ReadWrite, sizeInBytes);
+        }
+
+        public CLMem CreateBuffer(CLMemFlags flags, SizeT sizeInBytes)
+        {
+            CLMem buffer = OpenCLDriver.clCreateBuffer(ctx, flags, sizeInBytes, IntPtr.Zero, ref clError);
+            ThrowCLException(clError);
+
+            return buffer;
+        }
+
+        public CLMem CreateImage2D(CLMemFlags flags, CLImageFormat format, 
+            SizeT width, SizeT height, SizeT rowPitchInBytes)
+        {
+            CLMem image = OpenCLDriver.clCreateImage2D(ctx, flags, ref format, 
+                width, height, rowPitchInBytes, IntPtr.Zero, ref clError);
+            ThrowCLException(clError);
+
+            return image;
+        }
+
+        public CLMem CreateImage3D(CLMemFlags flags, CLImageFormat format,
+            SizeT width, SizeT height, SizeT depth,
+            SizeT rowPitchInBytes, SizeT slicePitchInBytes)
+        {
+            CLMem image = OpenCLDriver.clCreateImage3D(ctx, flags, ref format,
+                width, height, depth, rowPitchInBytes, slicePitchInBytes, 
+                IntPtr.Zero, ref clError);
+            ThrowCLException(clError);
+
+            return image;
+        }
+
+        public void RetainMemObject(CLMem obj)
+        {
+            clError = OpenCLDriver.clRetainMemObject(obj);
+            ThrowCLException(clError);
+        }
+
+        public void ReleaseMemObject(CLMem obj)
+        {
+            clError = OpenCLDriver.clReleaseMemObject(obj);
+            ThrowCLException(clError);
+        }
+
+        public CLImageFormat[] GetSupportedImageFormats(CLMemFlags flags, CLMemObjectType imageType)
+        {
+            uint numImageFormats = 0;
+            clError = OpenCLDriver.clGetSupportedImageFormats(ctx, flags, 
+                imageType, 0, IntPtr.Zero, ref numImageFormats);
+            ThrowCLException(clError);
+
+            if (numImageFormats < 1)
+            {
+                return new CLImageFormat[0];
+            }
+
+            CLImageFormat[] formats = new CLImageFormat[numImageFormats];
+            clError = OpenCLDriver.clGetSupportedImageFormats(ctx, flags,
+                imageType, numImageFormats, formats, ref numImageFormats);
+            ThrowCLException(clError);
+
+            return formats;
+        }
+        #endregion
+
         #region Properties
         /// <summary>
         /// Gets last OpenCL(TM) error that occured when calling an API function.
@@ -655,6 +724,155 @@ namespace CASS.OpenCL
             }
 
             return result;
+        }
+        #endregion
+
+        #region Memory Utilities
+        public static object GetMemObjectInfo(CLMem memobj, CLMemInfo info)
+        {
+            CLError error = CLError.Success;
+
+            // Define variables to store native information.
+            SizeT param_value_size_ret = 0;
+            IntPtr ptr = IntPtr.Zero;
+            object result = null;
+
+            // Get initial size of buffer to allocate.
+            error = OpenCLDriver.clGetMemObjectInfo(memobj, info, 0, IntPtr.Zero, ref param_value_size_ret);
+            ThrowCLException(error);
+
+            if (param_value_size_ret < 1)
+            {
+                return result;
+            }
+
+            // Allocate native memory to store value.
+            ptr = Marshal.AllocHGlobal(param_value_size_ret);
+
+            // Protect following statements with try-finally in case something 
+            // goes wrong.
+            try
+            {
+                // Get actual value.
+                error = OpenCLDriver.clGetMemObjectInfo(memobj, info,
+                    param_value_size_ret, ptr, ref param_value_size_ret);
+
+                //TODO: Add missing cases.
+
+                switch (info)
+                {
+                    case CLMemInfo.Type:
+                        result = (CLMemObjectType)Marshal.ReadInt32(ptr);
+                        break;
+                    case CLMemInfo.Flags:
+                        result = (CLMemFlags)Marshal.ReadInt64(ptr);
+                        break;
+                    case CLMemInfo.Size:
+                        result = new SizeT(Marshal.ReadIntPtr(ptr).ToInt64());
+                        break;
+                    case CLMemInfo.HostPtr:
+                        result = Marshal.ReadIntPtr(ptr);
+                        break;
+                    case CLMemInfo.MapCount:
+                        result = (uint)Marshal.ReadInt32(ptr);
+                        break;
+                    case CLMemInfo.ReferenceCount:
+                        result = (uint)Marshal.ReadInt32(ptr);
+                        break;
+                    case CLMemInfo.Context:
+                        result = Marshal.PtrToStructure(ptr, typeof(CLContext));
+                        break;
+                    case CLMemInfo.AssociatedMemObject:
+                        result = Marshal.PtrToStructure(ptr, typeof(CLMem));
+                        break;
+                    case CLMemInfo.Offset:
+                        result = new SizeT(Marshal.ReadIntPtr(ptr).ToInt64());
+                        break;
+                    case CLMemInfo.D3D10Resource:
+                        break;
+                }
+            }
+            finally
+            {
+                // Free native buffer.
+                Marshal.FreeHGlobal(ptr);
+            }
+
+            return result;
+        }
+
+        public static object GetImageInfo(CLMem memobj, CLImageInfo info)
+        {
+            CLError error = CLError.Success;
+
+            // Define variables to store native information.
+            SizeT param_value_size_ret = 0;
+            IntPtr ptr = IntPtr.Zero;
+            object result = null;
+
+            // Get initial size of buffer to allocate.
+            error = OpenCLDriver.clGetImageInfo(memobj, info, 0, IntPtr.Zero, ref param_value_size_ret);
+            ThrowCLException(error);
+
+            if (param_value_size_ret < 1)
+            {
+                return result;
+            }
+
+            // Allocate native memory to store value.
+            ptr = Marshal.AllocHGlobal(param_value_size_ret);
+
+            // Protect following statements with try-finally in case something 
+            // goes wrong.
+            try
+            {
+                // Get actual value.
+                error = OpenCLDriver.clGetImageInfo(memobj, info,
+                    param_value_size_ret, ptr, ref param_value_size_ret);
+
+                //TODO: Add missing cases.
+
+                switch (info)
+                {
+                    case CLImageInfo.Format:
+                        result = Marshal.PtrToStructure(ptr, typeof(CLImageFormat));
+                        break;
+                    case CLImageInfo.ElementSize:
+                        result = new SizeT(Marshal.ReadIntPtr(ptr).ToInt64());
+                        break;
+                    case CLImageInfo.RowPitch:
+                        result = new SizeT(Marshal.ReadIntPtr(ptr).ToInt64());
+                        break;
+                    case CLImageInfo.SlicePitch:
+                        result = new SizeT(Marshal.ReadIntPtr(ptr).ToInt64());
+                        break;
+                    case CLImageInfo.Width:
+                        result = new SizeT(Marshal.ReadIntPtr(ptr).ToInt64());
+                        break;
+                    case CLImageInfo.Height:
+                        result = new SizeT(Marshal.ReadIntPtr(ptr).ToInt64());
+                        break;
+                    case CLImageInfo.Depth:
+                        result = new SizeT(Marshal.ReadIntPtr(ptr).ToInt64());
+                        break;
+                    case CLImageInfo.D3D10SubResource:
+                        break;
+                }
+            }
+            finally
+            {
+                // Free native buffer.
+                Marshal.FreeHGlobal(ptr);
+            }
+
+            return result;
+        }
+
+        public static void SetMemObjectDestructorCallback(CLMem memobj, 
+            OpenCLDriver.DestructionFunction function, IntPtr userData)
+        {
+            CLError error = OpenCLDriver.clSetMemObjectDestructorCallback(memobj, function, userData);
+            ThrowCLException(error);
         }
         #endregion
 
