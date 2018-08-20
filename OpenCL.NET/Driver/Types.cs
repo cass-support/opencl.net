1e3401a99b51185b3d6af6842b561de426e1c15f
Mordechai Botrashvily <support@cass-hpc.com>
2018-08-20 11:00:36 +0000

    - Added bitfield for SVM memory flags, and removed definitions from normal mem flags.

--- OpenCL.NET/Driver/Types.cs
+++ OpenCL.NET/Driver/Types.cs
@@ -457,9 +457,21 @@ namespace CASS.OpenCL
         HostNoAccess = 1 << 9,
 
         /* 2.0 */
+        KernelReadAndWrite = 1 << 12,
+    }
+
+    /* 2.0 */
+    // cl_svm_mem_flags - bitfield
+    [Flags]
+    public enum CLSVMMemFlags : ulong
+    {
+        ReadWrite = 1 << 0,
+        WriteOnly = 1 << 1,
+        ReadOnly = 1 << 2,
+
+        /* 2.0 */
         SVMFineGrainBuffer = 1 << 10,   /* used by cl_svm_mem_flags only */
         SVMAtomics = 1 << 11,           /* used by cl_svm_mem_flags only */
-        KernelReadAndWrite = 1 << 12,
     }
 
     /* 1.2 */
